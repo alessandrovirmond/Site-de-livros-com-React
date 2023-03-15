303feb7d22a560467ea8befa00ee8587d0526544
Alessandro <avirmond2000@gmail.com>
2023-03-15 14:34:58 -0300

    Pesquisa de livros implementada, utilizando useState, onBlur, filter e maps

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import Header from './componentes/Header'
 import styled from 'styled-components';
+import Pesquisa from './componentes/pesquisa';
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
-li{
-  list-style: none;
-}
 `
 
 function App() {
   return (
     <AppContainer>
         <Header/>
+        <Pesquisa/>
     </AppContainer>
   ) 
 }
