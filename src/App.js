28b45dcda6dc5fb55a509b164dd0f3ed09ef587c
Alessandro <avirmond2000@gmail.com>
2023-03-17 16:39:34 -0300

    Props implementadas e projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './componentes/Header'
 import styled from 'styled-components';
 import Pesquisa from './componentes/pesquisa';
+import UltimosLancamentos from './componentes/ultimosLancamentos';
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -13,6 +14,7 @@ function App() {
     <AppContainer>
         <Header/>
         <Pesquisa/>
+        <UltimosLancamentos/>
     </AppContainer>
   ) 
 }
